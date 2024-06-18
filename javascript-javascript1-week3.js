3699a09e0357db4f07dccd7c9ae36653f2674a5d
Halyna Bondar <galya.marusyak@gmail.com>
2024-06-18 14:34:07 +0200

    When will we be there?

--- javascript-javascript1-week3.js
+++ javascript-javascript1-week3.js
@@ -15,3 +15,19 @@ const nameToRemove = names.splice(1, 1);
 
 console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
 
+// When will we be there?
+
+const travelInformation = {
+    speed: 50,
+    destinationDistance: 432,
+};
+
+const travelTime = calculateTime(travelInformation);
+console.log(travelTime); // 8 hours and 38 minutes
+
+function calculateTime(){
+    const time = travelInformation.destinationDistance / travelInformation.speed;
+    const hours = Math.floor(time);
+    const minutes= Math.floor((time - hours) * 60);
+    return `${hours} hours and ${minutes} minutes`;
+}
