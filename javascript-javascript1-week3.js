ade49bf235051ec25774b361e36b11ee9f6f0335
Halyna Bondar <galya.marusyak@gmail.com>
2024-06-18 14:03:33 +0200

    Item array removal

---
+++ javascript-javascript1-week3.js
@@ -0,0 +1,17 @@
+// Its warmup time!
+// Item array removal
+
+const names = [
+    "Peter",
+    "Ahmad",
+    "Yana",
+    "kristina",
+    "Rasmus",
+    "Samuel",
+    "katrine",
+    "Tala",
+];
+const nameToRemove = names.splice(1, 1);
+
+console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+
