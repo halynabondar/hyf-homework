f3bfcf759d2d90b41cc6b1459a2c63be925d052d
Halyna Bondar <galya.marusyak@gmail.com>
2024-08-08 14:31:07 +0200

    Create my own json file

---
+++ javascript/javascript3/week1/script.js
@@ -0,0 +1,101 @@
+// Create your own json file
+
+{
+    "genres": [
+    {
+        "name": "Rock",
+        "subgenres": [
+            "Classic Rock",
+            "Hard Rock",
+            "Alternative Rock",
+            "Punk Rock",
+            "Indie Rock"
+        ]
+    },
+    {
+        "name": "Pop",
+        "subgenres": [
+            "Synth-pop",
+            "Teen Pop",
+            "Electropop",
+            "Dance-pop"
+        ]
+    },
+    {
+        "name": "Hip Hop",
+        "subgenres": [
+            "East Coast Hip Hop",
+            "West Coast Hip Hop",
+            "Trap",
+            "Boom Bap"
+        ]
+    },
+    {
+        "name": "Jazz",
+        "subgenres": [
+            "Swing",
+            "Bebop",
+            "Free Jazz",
+            "Cool Jazz",
+            "Smooth Jazz"
+        ]
+    },
+    {
+        "name": "Classical",
+        "subgenres": [
+            "Baroque",
+            "Romantic",
+            "Contemporary Classical",
+            "Opera",
+            "Chamber Music"
+        ]
+    },
+    {
+        "name": "Electronic",
+        "subgenres": [
+            "House",
+            "Techno",
+            "Trance",
+            "Dubstep",
+            "Drum and Bass"
+        ]
+    },
+    {
+        "name": "Country",
+        "subgenres": [
+            "Bluegrass",
+            "Honky Tonk",
+            "Outlaw Country",
+            "Country Pop",
+            "Alternative Country"
+        ]
+    },
+    {
+        "name": "Reggae",
+        "subgenres": [
+            "Roots Reggae",
+            "Dub",
+            "Dancehall",
+            "Reggaeton"
+        ]
+    },
+    {
+        "name": "Blues",
+        "subgenres": [
+            "Delta Blues",
+            "Chicago Blues",
+            "Electric Blues",
+            "Blues Rock"
+        ]
+    },
+    {
+        "name": "R&B",
+        "subgenres": [
+            "Contemporary R&B",
+            "Soul",
+            "Neo-Soul",
+            "Funk"
+        ]
+    }
+]
+}
