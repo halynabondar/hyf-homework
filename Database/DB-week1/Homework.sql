736e33e44689c6eb995cf6abf48f66b366ce0844
Halyna Bondar <galya.marusyak@gmail.com>
2024-08-27 20:33:12 +0200

    Class exercises

--- Database/DB-week1/Homework.sql
+++ Database/DB-week1/Homework.sql
@@ -10,12 +10,12 @@ WHERE due_date IS NULL;
 -- 3. Find all the tasks that are marked as done
 SELECT *
 FROM task
-WHERE status_id = 'Done';
+WHERE status_id = 3;
 
 -- 4. Find all the tasks that are not marked as done
 SELECT *
 FROM task
-WHERE status_id != 'Done';
+WHERE status_id != 3;
 
 -- 5. Get all the tasks, sorted with the most recently created first
 SELECT *
@@ -25,7 +25,8 @@ ORDER BY created DESC;
 -- 6. Get the single most recently created task
 SELECT *
 FROM task
-ORDER BY created_at DESC LIMIT 1;
+ORDER BY created DESC
+LIMIT 1;
 
 -- 7. Get the title and due date of all tasks where the title or description contains database
 SELECT title,
@@ -37,10 +38,10 @@ WHERE title LIKE '%database%'
 -- 8. Get the title and status (as text) of all tasks
 SELECT title,
        status_id
-FROM task
+FROM task;
 
 -- 9. Get the name of each status, along with a count of how many tasks have that status
-SELECT status, COUNT(*) AS task_count
+SELECT status_id, COUNT(*) AS task_count
 FROM task
 GROUP BY status_id;
 
@@ -49,3 +50,66 @@ SELECT status_id, COUNT(*) AS task_count
 FROM task
 GROUP BY status_id
 ORDER BY task_count DESC;
+
+
+-- Class exercises
+-- Implement the following queries:
+
+-- Select the names and phones of all users;
+SELECT name,
+       phone
+FROM user;
+
+-- Select the name of the user with id=10;
+SELECT name
+FROM user
+WHERE id = 10;
+
+-- Find how many users exist in the database;
+SELECT COUNT(*) AS user_count
+FROM mysql.user;
+
+-- Select the names of the first 5 users in the database;
+SELECT name
+FROM user
+LIMIT 5;
+
+-- Select the names of the last 3 users in the database;
+SELECT name
+FROM user
+ORDER BY name DESC
+LIMIT 3;
+
+-- Sum all the ids in the user table;
+SELECT SUM(id) AS total_id_sum
+FROM user;
+
+-- Select all users and order them alphabetically by name;
+SELECT *
+FROM user
+ORDER BY name;
+
+-- Find all tasks that include SQL either on the title or on the description;
+SELECT *
+FROM task
+WHERE title LIKE '%SQL%'
+   OR description LIKE '%SQL%';
+
+-- Find the title of all tasks that the user Maryrose is responsible for;
+SELECT t.title
+FROM task t
+         JOIN user u ON t.user_id = u.id
+WHERE u.name = 'Maryrose';
+
+-- Find how many tasks each user is responsible for;
+SELECT u.name, COUNT(*) AS task_count
+FROM task t
+         JOIN user u ON t.user_id = u.id
+GROUP BY u.name;
+
+-- Find how many tasks with a status=Done each user is responsible for;
+SELECT u.name, COUNT(*) AS done_task_count
+FROM task t
+         JOIN user u ON t.user_id = u.id
+WHERE t.status_id = 3
+GROUP BY u.name;
