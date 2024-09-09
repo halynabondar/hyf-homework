385977f9c8ccc1edac5cbeee6f5a0ba834a12b13
Halyna Bondar <galya.marusyak@gmail.com>
2024-09-09 14:12:11 +0200

    Additional queries added

--- Database/DB-week3/Queries.sql
+++ Database/DB-week3/Queries.sql
@@ -95,3 +95,52 @@ WHERE id = 1;
 DELETE
 FROM Review
 WHERE id = 1;
+
+-- Additional queries --
+
+-- Get meals that has a price smaller than a specific price fx 90
+SELECT *
+FROM Meal
+WHERE price < 90;
+
+-- Get meals that still has available reservations
+SELECT m.id, m.title, m.max_reservations, COALESCE(reserved_count, 0) AS reserved_count
+FROM Meal m
+         LEFT JOIN (SELECT meal_id, COUNT(*) as reserved_count
+                    FROM Reservation
+                    GROUP BY meal_id) r ON m.id = r.meal_id
+WHERE COALESCE(reserved_count, 0) < m.max_reservations;
+
+-- Get meals that partially match a title. Rød grød med will match the meal with the title Rød grød med fløde
+SELECT *
+FROM Meal
+WHERE title LIKE 'Rød grød med%';
+
+-- Get meals that has been created between two dates
+SELECT *
+FROM Meal
+WHERE created_date BETWEEN '2024-01-01' AND '2024-12-31';
+
+-- Get only specific number of meals fx return only 5 meals
+SELECT *
+FROM Meal
+LIMIT 5;
+
+-- Get the meals that have good reviews
+SELECT m.*
+FROM Meal m
+         JOIN Review r ON m.id = r.meal_id
+WHERE r.stars >= 4;
+
+-- Get reservations for a specific meal sorted by created_date
+SELECT *
+FROM Reservation
+WHERE meal_id = 'specific_meal_id'
+ORDER BY created_date DESC;
+
+-- Sort all meals by average number of stars in the reviews
+SELECT m.*, AVG(r.stars) AS average_rating
+FROM Meal m
+         JOIN Review r ON m.id = r.meal_id
+GROUP BY m.id, m.title, m.description, m.created_date
+ORDER BY average_rating DESC;
